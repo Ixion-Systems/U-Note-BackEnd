f66a82445c0c95487982e14e2e04f722bca36415
NicoWASD <nicolasneilpereira@gmail.com>
2026-06-08 11:33:09 -0300

    Modulo funcional del APP

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -11,8 +12,12 @@ app.use(express.json({ extended: false }));
 // Test route
 app.get('/api/health', (req, res) => res.send('API running'));
 
-// Authentication Routes
+// Static route for PDFs
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 const PORT = process.env.PORT || 5000;
 
